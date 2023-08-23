bfa632eb3eacb52a44bae288a8507010f02201c6
Duda Melim <137728498+dudelim@users.noreply.github.com>
2023-08-23 15:51:36 -0300

    adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,23 +4,33 @@ print("------------------------------------")
 print("Bem vindo(a) ao jogo de adivinhação!")
 print("------------------------------------")
 
-numero_random = round(random.random() * 100)
-int (numero_random)
-total_tentativas = 3
-rodada = 1
+numero_random = random.randrange(1,101)
+total_tentativas = 0
 
-while (rodada <= total_tentativas):
+print("Selecione o nível de dificuldade")
+print("(1) Fácil (2) Médio (3) Difícil")
+nivel = int(input("Nível selecionado: "))
+
+if (nivel == 1):
+  total_tentativas = 20
+elif(nivel == 2):
+  total_tentativas = 10
+elif(nivel == 3):
+  total_tentativas = 5
+
+for rodada in range(1, total_tentativas + 1):
   print("Tentativa {} de {}".format (rodada, total_tentativas))
 
-  chute_str = input("Digite um número: ")
-  print("Número digitado: ", chute_str)
-  chute = int(chute_str)
+  chute = int(input("Digite um número entre 1 e 100: "))
+  print("Número digitado: ", chute)
+  
+
+  if(chute < 1 or chute > 100):
+    print("Você deve digitar um número entre 1 e 100!")
 
   acertou = numero_random == chute
   maior = chute > numero_random
   menor = chute < numero_random
-
-  rodada = rodada + 1
 
   if (acertou):
     print("Você acertou!")
